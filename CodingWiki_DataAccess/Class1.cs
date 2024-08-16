d550367a80591af8288f2887275b3dbb26512d93
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2024-08-16 15:45:26 -0600

    update

--- CodingWiki_DataAccess/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CodingWiki_DataAccess
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ CodingWiki_Model/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_Model.Models
+{
+    public class Book
+    {
+        public int IdBook { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public Double Price { get; set; }
+            
+    }
+}
