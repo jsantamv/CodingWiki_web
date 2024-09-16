ceaf5d08657accf3e6b9491739ce99e9f8708170
Juan Carlos Santamaria <jsantamv@live.com>
2024-09-15 19:44:08 -0600

    update

---
+++ CodingWiki_DataAccess/Migrations/20240915175953_update column name.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatecolumnname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres");
+
+            migrationBuilder.RenameTable(
+                name: "Genres",
+                newName: "tb_genres");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_tb_genres",
+                table: "tb_genres",
+                column: "GenreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_tb_genres",
+                table: "tb_genres");
+
+            migrationBuilder.RenameTable(
+                name: "tb_genres",
+                newName: "Genres");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres",
+                column: "GenreId");
+        }
+    }
+}
