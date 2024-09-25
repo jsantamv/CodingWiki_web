1f676b12dc3e85a54112251cc75e380e9164cab6
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2024-09-24 21:44:28 -0600

    update messeges

---
+++ CodingWiki_web/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_web.Controllers;
+
+public class CategoryController(ApplicationDbContext db) : Controller
+{
+    private readonly ApplicationDbContext _db = db;
+
+    public IActionResult Index()
+    {
+        var categories = _db.Categories.ToList();
+        return View(categories);
+    }
+}
